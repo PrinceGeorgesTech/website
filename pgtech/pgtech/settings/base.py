8b16e5739657289fa7d95da144fe6686590b3c53
geramirez <gabriel.e.ramirez@gmail.com>
2017-05-06 22:52:59 -0400

    Dockerizing application

--- pgtech/pgtech/settings/base.py
+++ pgtech/pgtech/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 from __future__ import absolute_import, unicode_literals
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -93,12 +94,8 @@ WSGI_APPLICATION = 'pgtech.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
